350a7cbf74a12cc5c961ceb5bb068b89811ca47c
Vitaly Belman <vitalyb+radio@gmail.com>
2015-11-12 20:55:13 +0200

    using wrapped_sound_manager as an abstraction for SoundManger2 and sound + adding ability to preload sound

--- www/src/actions.js
+++ www/src/actions.js
@@ -2,6 +2,7 @@ import { soundManager } from 'soundmanager2';
 import { pushState } from 'redux-router';
 import ajaxConstructor from './ajax'
 import { dispatchContainer } from './dispatch'
+import { getSoundBySong, loadSound, WrappedSound } from './wrapped_sound_manager'
 
 export const FETCH_NEXT_SONG_DETAILS_ASYNC = 'FETCH_NEXT_SONG_DETAILS_ASYNC';
 
@@ -11,8 +12,7 @@ export const SOUND_PAUSE = 'SOUND_PAUSE';
 export const SOUND_FINISHED = 'SOUND_FINISHED';
 export const SOUND_PLAY_ERROR = 'SOUND_PLAY_ERROR';
 
-const SERVER_ADDRESS = window.location.protocol + "//" + window.location.hostname+":5000";
-const MUSIC_ADDRESS = window.location.protocol + "//" + window.location.hostname+":16768";
+const SERVER_ADDRESS = window.location.protocol + "//" + window.location.hostname + ":5000";
 
 soundManager.setup({
     url: require("file!../lib/swf/soundmanager2.swf"),
@@ -22,67 +22,38 @@ soundManager.setup({
     html5PollingInterval: 50
 });
 
+// redirect to login page on any 401
 let ajax = ajaxConstructor(SERVER_ADDRESS, function (response) {
-    if(response.status == 401) {
+    if (response.status == 401) {
         dispatchContainer.dispatch(pushState(null, '/login'))
     }
 
     return response;
 });
 
-function _loadSound(dispatch, song) {
-    // TODO: Handle failures
-    let songId = song.id;
 
-    return new Promise(function (resolve, reject) {
-        soundManager.createSound({
-            id: songId, // optional: provide your own unique id
-            url: MUSIC_ADDRESS + "/" + song.location,
-            autoLoad: true,
-            // Every song that was loaded, should have the following events
-            // NOTE: This is a bit of a leaky abstraciton. It is tricky to decouple onfinish from this function since
-            // events can be subscribed to only on creation or during play.
-            onload: function () {
-
-                if (this.duration == 0) {
-                    reject(new Error(`Song ${songId} duration after load was 0`))
-                }
-
-                this.onPosition(this.duration - 1000, () => {
-                    console.log(`SUCCESS song ${songId}: almost finished. marking as played`);
-                    _markSongAsPlayed(songId, dispatch)
-                });
-
-                console.log(`SUCCESS song ${songId}: loaded`);
-                resolve(this);
-
-            },
-            onfinish: function () {
-                console.log(`SUCCESS song ${songId}: finished`);
-                dispatch({type: SOUND_FINISHED, songId: songId})
-            }
-        })
-    })
-}
-
-function _fetchNextSongDetails(playlistName, dispatch) {
-    dispatch({type: FETCH_NEXT_SONG_DETAILS_ASYNC, inProgress: true});
+function _fetchNextSongDetails(playlistName) {
+    dispatchContainer.dispatch({type: FETCH_NEXT_SONG_DETAILS_ASYNC, inProgress: true});
 
     return ajax.get(`/playlist/${playlistName}/next`)
         .then(response => response.json().then(json => ({json, response})))
         .then(({ json, response }) => {
-            dispatch({type: FETCH_NEXT_SONG_DETAILS_ASYNC, ok: response.ok, json})
+            dispatchContainer.dispatch({type: FETCH_NEXT_SONG_DETAILS_ASYNC, ok: response.ok, json});
             return json.next;
         })
-        .catch((ex) => {
-            dispatch({type: FETCH_NEXT_SONG_DETAILS_ASYNC, ok: false, json: {error: "Connection error"}})
+        .catch(() => {
+            dispatchContainer.dispatch({
+                type: FETCH_NEXT_SONG_DETAILS_ASYNC,
+                ok: false,
+                json: {error: "Connection error"}
+            })
         });
 }
 
-function _markSongAsPlayed(songId, dispatch) {
+function _markSongAsPlayed(songId) {
     console.log(`IN PROGESS song ${songId}: mark as played`);
     return ajax.post(`/song/${songId}/last-played`)
-        .then(response => {
+        .then(() => {
             console.log(`SUCCESS song ${songId}: mark as played`)
         })
         .catch((ex) => {
@@ -91,66 +62,85 @@ function _markSongAsPlayed(songId, dispatch) {
 }
 
 // Tries to get the sound from SoundManager, if fails, tries to load it
-function _getOrLoadSound(dispatch, song) {
+function _getOrLoadSound(song) {
     // TODO: Handle failures
-    let songId = song.id;
-
-    return Promise.resolve(soundManager.getSoundById(songId))
+    return Promise.resolve(getSoundBySong(song))
         .then(function (sound) {
             if (sound && sound.loaded) {
                 return sound;
             } else {
-                dispatch({type: SOUND_LOADING, songId: songId});
-                return _loadSound(dispatch, song);
+                dispatchContainer.dispatch({type: SOUND_LOADING, songId: song.id});
+                return loadSound(song);
             }
         });
 }
 
-function _playToggle(dispatch, song, playOptions) {
-    let songId = song.id;
+// resolves to the played/paused sound on success
+function _playToggle(wrappedSound, playOptions) {
 
-    return _getOrLoadSound(dispatch, song)
-        .then(function (sound) {
-            if (!sound.loaded) {
-                throw new Error("Sound should be loadded at this point: " + sound.id)
-            }
+    try {
+        if (!wrappedSound.loaded) {
+            throw new Error("Sound should be loaded at this point: " + wrappedSound.song.id)
+        }
 
-            if (sound.playState == 0 || sound.paused) {
-                sound.play(playOptions);
-                dispatch({type: SOUND_PLAY, songId: songId})
-            } else {
-                sound.pause();
-                dispatch({type: SOUND_PAUSE, songId: songId})
-            }
-        })
-        .catch(function(err) {
-            dispatch({type: SOUND_PLAY_ERROR, songId: songId})
+        if (wrappedSound.playState == 0 || wrappedSound.paused) {
+            wrappedSound.play(playOptions);
+            dispatchContainer.dispatch({type: SOUND_PLAY, songId: wrappedSound.song.id})
+        } else {
+            wrappedSound.pause();
+            dispatchContainer.dispatch({type: SOUND_PAUSE, songId: wrappedSound.song.id})
+        }
 
-            throw err;
-        });
+        return wrappedSound.song.id;
+    }
+    catch (err) {
+        dispatchContainer.dispatch({type: SOUND_PLAY_ERROR, songId: wrappedSound.song.id});
+
+        throw err;
+    }
 }
 
-function _playPlaylist(dispatch, playlistName) {
-    _fetchNextSongDetails(playlistName, dispatch)
+function _playPlaylist(playlistName) {
+    _fetchNextSongDetails(playlistName)
         .then(function (nextSong) {
-            return _playToggle(dispatch, nextSong, {
-                onfinish: () => {
-                    _playPlaylist(dispatch, playlistName);
-                }
-            }).catch(function (err) {
-                console.log(`ERROR failed to play song ${nextSong.id}: Will mark it as read and proceed to next one`);
-                _markSongAsPlayed(nextSong.id, dispatch).then(function() {
-                    console.log(`Marked as read - Continue to play playlist: ${playlistName}`);
-                    _playPlaylist(dispatch, playlistName);
-                });
-            });
-
+            return _getOrLoadSound(nextSong);
         })
+        .then(function (wrappedSound) {
+            return _playToggle(wrappedSound, {
+                onPosition: {
+                    position: wrappedSound.duration - 5000,
+                    callback: () => {
+                        console.log(`SUCCESS song ${wrappedSound.song.id}: almost finished. marking as played and preloading next song`);
+                        return _markSongAsPlayed(wrappedSound.song.id)
+                            .then(function () {
+                                return _fetchNextSongDetails(playlistName)
+                            }).then(function (preloadedSong) {
+                                console.log(`next song is ${preloadedSong.id}. Preloading...`);
+                                return _getOrLoadSound(preloadedSong);
+                            }).then(function (preloadedSound) {
+                                console.log(`preloaded sound for song: ${preloadedSound.song.id}`);
+                            })
+                    }
+                },
+                onfinish: () => {
+                    console.log(`song finished: ${wrappedSound.song.id}. starting playlist: ${playlistName}`);
+                    _playPlaylist(playlistName);
+                }
+            })
+        }).catch(function () {
+        console.log(`ERROR failed to play song ${wrappedSound.song.id}: Will mark it as read and proceed to next one`);
+        _markSongAsPlayed(wrappedSound.song.id).then(function () {
+            console.log(`Marked as read - Continue to play playlist: ${playlistName}`);
+            _playPlaylist(playlistName);
+        });
+    })
 }
 
 export function playToggle(song, playOptions) {
-    return function (dispatch) {
-        _playToggle(dispatch, song, playOptions).catch(function(err) {
+    return function () {
+        _getOrLoadSound(song).then(function (wrappedSound) {
+            _playToggle(wrappedSound, playOptions);
+        }).catch(function (err) {
             console.error(`ERROR - Failed to play sound: ${err}`);
         });
     }
@@ -165,20 +155,20 @@ export function fastForward(songId) {
 }
 
 export function playPlaylist(playlistName) {
-    return function (dispatch) {
-        _playPlaylist(dispatch, playlistName);
+    return function () {
+        _playPlaylist(playlistName);
     }
 }
 
 export function fetchNextSongDetails(playlistName) {
-    return function (dispatch) {
-        _fetchNextSongDetails(playlistName, dispatch);
+    return function () {
+        _fetchNextSongDetails(playlistName);
     }
 }
 
 export function login(password) {
 
     return ajax.post("/access-token", {body: {password}}).
-        then(response => response.json().then(json => json))
+    then(response => response.json().then(json => json))
 
-};
+}
---
+++ www/src/wrapped_sound_manager.js
@@ -0,0 +1,106 @@
+import { soundManager } from 'soundmanager2';
+
+const MUSIC_ADDRESS = window.location.protocol + "//" + window.location.hostname + ":16768";
+
+console.info(`running soundManager setup`);
+soundManager.setup({
+    url: require("file!../lib/swf/soundmanager2.swf"),
+    flashVersion: 9, // optional: shiny features (default = 8)
+    // optional: ignore Flash where possible, use 100% HTML5 mode
+    preferFlash: false,
+    html5PollingInterval: 50
+});
+
+export function getSoundBySong(song) {
+    let songId = song.id;
+
+    let sound = soundManager.getSoundById(songId);
+    if(sound) {
+        sound = new WrappedSound(song, sound);
+    }
+
+    return sound;
+}
+
+// List of loading songs promises
+let loadingSongs = {};
+
+// returns promise.
+// resolves to WrappedSound
+export function loadSound(song) {
+    let songId = song.id;
+    console.info(`request to load song: ${songId}`)
+
+    let loadingPromise = null;
+    if(loadingSongs[songId]) {
+        console.info(`sound is already being created, returning existing promise: ${songId}`)
+        loadingPromise = loadingSongs[songId];
+    } else {
+        console.info(`starting createSound for song: ${songId}`);
+        loadingPromise = new Promise(function (resolve, reject) {
+            soundManager.createSound({
+                id: songId, // optional: provide your own unique id
+                url: MUSIC_ADDRESS + "/" + song.location,
+                autoLoad: true,
+                // Every song that was loaded, should have the following events
+                // NOTE: This is a bit of a leaky abstraciton. It is tricky to decouple onfinish from this function since
+                // events can be subscribed to only on creation or during play.
+                onload: function (success) {
+                    delete loadingSongs[songId];
+
+                    console.info(`SoundManager loadSound finished. Success: ${success}`);
+                    if (this.duration == 0) {
+                        reject(new Error(`Song ${songId} duration after load was 0`))
+                    }
+
+                    if (!this.loaded) {
+                        reject(new Error(`Song ${songId} loaded is false`))
+                    }
+
+                    console.log(`SUCCESS song ${songId}: loaded`);
+                    resolve(new WrappedSound(song, this));
+
+                }
+            })
+        });
+
+        loadingSongs[songId] = loadingPromise;
+    }
+
+    return loadingPromise;
+}
+
+// wraps SoundManager2 sound object
+export class WrappedSound {
+
+    constructor(song, sound) {
+        this.song = song;
+        this.sound = sound;
+    }
+
+    // options:
+    // onPosition - {position: X, callback: Y}. Called when playing reaches this position. Callback is discarded when the song is paused/stopped.
+    // Other options are avialable per docs of soundManager sound.play function.
+    play(options = {}) {
+        if(options.onPosition) {
+
+            let {position, callback} = options.onPosition;
+
+            console.info(`adding a callback on position: ${position}`);
+            // clears all callbacks on this position, prevents duplicates callbacks on play/pause
+            this.sound.clearOnPosition(position);
+            this.sound.onPosition(position, callback);
+
+            delete options.onPosition;
+        }
+
+        this.sound.play(options);
+    }
+
+    pause() { this.sound.pause(); }
+    get loaded() { return this.sound.loaded; }
+    get playState() { return this.sound.playState; }
+    get paused() { return this.sound.paused; }
+    get duration() { return this.sound.duration; }
+
+}
