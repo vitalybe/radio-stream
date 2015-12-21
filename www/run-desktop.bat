49422b55e00f99f3c7a4c43ef13a7e38c410a2c8
Vitaly Belman <vitalyb+radio@gmail.com>
2015-12-21 21:57:42 +0200

    run scripts for desktop and web

--- www/run-desktop.bat
+++ www/run-desktop.bat
@@ -1,5 +1,6 @@
 cd..
-ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat & cd www & npm start" -cur_console:s50V:n:t:"web":C:"c:\Users\Vitaly\Dropbox\Utils\Cmder\icons\webpack.ico"
+ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat & cd www & npm run hot-server-desktop" -cur_console:s50V:n:t:"web-server":C:"c:\Users\Vitaly\Dropbox\Utils\Cmder\icons\webpack.ico"
+ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat & cd www & npm run start-hot" -cur_console:s50V:n:t:"desktop":C:"c:\Users\Vitaly\Dropbox\Utils\Cmder\icons\webpack.ico"
 ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat & cd server & api.py" -cur_console:s50V:n:t:"backend":C:"c:\Users\Vitaly\Dropbox\Utils\Cmder\icons\flask.ico"
 taskkill /f /im nginx.exe
 ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat & cd c:\nginx\ & nginx" -cur_console:s50V:n:t:"nginx":C:"c:\Users\Vitaly\Dropbox\Utils\Cmder\icons\nginx.ico"
