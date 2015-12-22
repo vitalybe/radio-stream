20c90a18cfea3db89b4b070f5d7ee96e8582bdd3
Vitaly Belman <vitalyb+radio@gmail.com>
2015-12-22 22:26:00 +0200

    Revert "broken - native getIdleTime. works in node, but having native binding problems under electron :("
    This reverts commit 8fbf9fdc6a0aefd1892168aef5c21fdfb4b7a0fe.

--- www/main.js
+++ www/main.js
@@ -3,65 +3,21 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 const crashReporter = electron.crashReporter;
+const shell = electron.shell;
 const globalShortcut = electron.globalShortcut;
 const ipcMain = electron.ipcMain;
 
+let menu;
+let template;
 let mainWindow = null;
+
 
 crashReporter.start();
 
-// stolen from: https://github.com/atom/electron/issues/1528
-function getIdleTime() {
-    let ref = require('ref');
-    let refStruct = require('ref-struct');
-    let refArray = require('ref-array');
-    let ffi = require('ffi');
-
-    var intPtr = null;
-    var boolPtr = null;
-    var LASTINPUTINFO = null;
-    var pLASTINPUTINFO = null;
-    var user32 = null;
-    var kernel32 = null;
-
-    function setupWindowsLibs() {
-        intPtr = intPtr || ref.refType(ref.types.int32);
-        boolPtr = boolPtr || ref.refType(ref.types.bool);
-
-        LASTINPUTINFO = LASTINPUTINFO || refStruct({
-                cbSize: ref.types.int32,
-                dwTime: ref.types.uint32
-            });
-
-        pLASTINPUTINFO = pLASTINPUTINFO || ref.refType(LASTINPUTINFO);
-
-        kernel32 = ffi.Library('kernel32', {'GetTickCount': ['int', null]});
-        user32 = user32 || ffi.Library('user32', {
-                'GetLastInputInfo': ['int', [pLASTINPUTINFO]]
-            });
-    }
-
-    function getIdleTimeInMs() {
-      setupWindowsLibs();
-
-      let result = new LASTINPUTINFO();
-      result.cbSize = LASTINPUTINFO.size;
-
-      let failed = (user32.GetLastInputInfo(result.ref()) === 0);
-
-      if (failed) {
-        throw new Error("Couldn't get idle time");
-      }
-
-      return kernel32.GetTickCount() - result.dwTime;
-    }
-
-    return getIdleTimeInMs()
-}
-
-// Enable dev-tools
 require('electron-debug')();
+
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
@@ -71,6 +27,7 @@ app.on('will-quit', function () {
     // Unregister all shortcuts.
     globalShortcut.unregisterAll();
 });
+
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({width: 1024, height: 728});
@@ -86,14 +43,13 @@ app.on('ready', () => {
     });
 
     ipcMain.on('song-changed', function (event, newSong) {
-        if (newSong) {
+        if(newSong) {
             mainWindow.setTitle(`${newSong.name} - ${newSong.artist} - ${originalTitle}`);
         } else {
             mainWindow.setTitle(originalTitle);
         }
     });
 
-    setInterval(() => log(`Idle time: ${getIdleTime()}`), 1000);
 
     if (process.env.HOT) {
         mainWindow.loadURL(`file://${__dirname}/app/hot-dev-index.html`);
