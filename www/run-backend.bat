d08e86f6d718dab2d5140b8e9103e347c03de7d7
Vitaly Belman <vitalyb+radio@gmail.com>
2015-12-29 22:23:55 +0200

    making it work on htpc computer

--- www/run-backend.bat
+++ www/run-backend.bat
@@ -1,8 +1,7 @@
-ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat & cd server & api.py" -cur_console:s50V:n:t:"backend":C:"c:\Users\Vitaly\Dropbox\Utils\Cmder\icons\flask.ico"
-taskkill /f /im nginx.exe
-ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat & cd c:\nginx\ & nginx" -cur_console:s50V:n:t:"nginx":C:"c:\Users\Vitaly\Dropbox\Utils\Cmder\icons\nginx.ico"
+ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat & cd server & api.py" -cur_console:s50V:n:t:"backend":C:"c:\Users\%username%\Dropbox\Utils\Cmder\icons\flask.ico"
+ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat & taskkill /f /im nginx.exe & cd c:\Users\%username%\Dropbox\Utils\nginx\ & nginx" -cur_console:s50V:n:t:"nginx":C:"c:\Users\%username%\Dropbox\Utils\Cmder\icons\nginx.ico"
 
 cd www
-ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat" -new_console:t:"cmd":C:"c:\Users\Vitaly\Dropbox\Utils\Cmder\Cmder.exe"
+ConEmu.exe /single /cmd cmd /k "%ConEmuDir%\..\init.bat" -new_console:t:"cmd":C:"c:\Users\%username%\Dropbox\Utils\Cmder\Cmder.exe"
 sleep 1
 exit
