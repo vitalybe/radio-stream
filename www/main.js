fb5408cca85fa31f62edf02b6d1492df27a78d44
Vitaly Belman <vitalyb+radio@gmail.com>
2016-02-06 14:34:00 +0200

    main.js - refactoring for readability

--- www/main.js
+++ www/main.js
@@ -1,61 +1,49 @@
 /* eslint strict: 0 */
 'use strict';
+require('electron-debug')();
 const path = require('path');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const Menu = electron.Menu;
 const crashReporter = electron.crashReporter;
-const shell = electron.shell;
-const globalShortcut = electron.globalShortcut;
 const ipcMain = electron.ipcMain;
 
-let menu;
-let template;
 let mainWindow = null;
 
-
-crashReporter.start();
-
-require('electron-debug')();
-
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-});
-
-app.on('will-quit', function () {
-    // Unregister all shortcuts.
-    globalShortcut.unregisterAll();
-});
+function log(msg) {
+    mainWindow.webContents.send('log', msg);
+}
 
 
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({width: 1024, height: 728});
-    let originalTitle = "Music stream";
-
-    function log(msg) {
-        mainWindow.webContents.send('log', msg);
-    }
-
-    globalShortcut.register('Super+Alt+CmdOrCtrl+Shift+O', function () {
-        log('play/pause toggle key pressed');
-        mainWindow.webContents.send('playPauseToggle');
+function handleQuitting() {
+    mainWindow.on('closed', () => {
+        mainWindow = null;
     });
 
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') app.quit();
+    });
+    app.on('will-quit', function () {
+        // Unregister all shortcuts.
+        globalShortcut.unregisterAll();
+    });
+}
+function handleTitleChanges() {
+    let originalTitle = "Music stream";
+
     ipcMain.on('song-changed', function (event, newSong) {
-        if(newSong) {
+        if (newSong) {
             mainWindow.setTitle(`${newSong.name} - ${newSong.artist} - ${originalTitle}`);
         } else {
             mainWindow.setTitle(originalTitle);
         }
     });
-
-
+}
+function handleUseIdling() {
     var exec = require('child_process').exec;
     var cmd = path.join("lib", "binary", "win32-GetIdleTime.exe");
 
-    setInterval(()=>{
+    setInterval(()=> {
         exec(cmd, function (error, stdout, stderr) {
             log(`Idle time: ${stdout}`);
             mainWindow.webContents.send('idle', stdout);
@@ -63,18 +51,28 @@ app.on('ready', () => {
     }, 60000);
 
 
+}
+function handleGlobalShortcuts() {
+    const globalShortcut = electron.globalShortcut;
+    globalShortcut.register('Super+Alt+CmdOrCtrl+Shift+O', function () {
+        log('play/pause toggle key pressed');
+        mainWindow.webContents.send('playPauseToggle');
+    });
+}
+
+app.on('ready', () => {
+    crashReporter.start();
+    mainWindow = new BrowserWindow({width: 1024, height: 728});
+
+    handleTitleChanges();
+    handleUseIdling();
+    handleGlobalShortcuts();
+    handleQuitting();
+
     if (process.env.HOT) {
         mainWindow.loadURL(`file://${__dirname}/app/hot-dev-index.html`);
     } else {
         mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
-    }
-
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
-
-    if (process.env.NODE_ENV === 'development') {
-        mainWindow.openDevTools();
     }
 
 });
