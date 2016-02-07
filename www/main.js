9c50c12eb3e6f6cf1d000c9f93116f9c17afa9da
Vitaly Belman <vitalyb+radio@gmail.com>
2016-02-07 22:42:29 +0200

    keyboard invoked toast with song details

--- www/main.js
+++ www/main.js
@@ -3,10 +3,14 @@
 require('electron-debug')();
 const path = require('path');
 const electron = require('electron');
+const notifier = require('node-notifier');
+const moment = require('moment');
+
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const crashReporter = electron.crashReporter;
 const ipcMain = electron.ipcMain;
+
 
 let mainWindow = null;
 
@@ -50,11 +54,37 @@ function handleUseIdling() {
 }
 function handleGlobalShortcuts() {
     const globalShortcut = electron.globalShortcut;
+    let currentSong = null;
+
+    ipcMain.on('song-changed', function (event, newSong) {
+        currentSong = newSong;
+    });
+
 
     globalShortcut.register('Alt+Ctrl+Home', function () {
         log('play/pause toggle key pressed');
         mainWindow.webContents.send('playPauseToggle');
     });
+
+    globalShortcut.register('Alt+Ctrl+Shift+Home', function () {
+        log('show info pressed');
+        const Growl = require('node-notifier').Growl;
+
+        var notifier = new Growl({
+            name: 'Music Stream', // Defaults as 'Node'
+            host: 'localhost',
+            port: 23053
+        });
+
+        if(currentSong) {
+            let lastPlayed = moment.unix(currentSong.lastPlayed).fromNow();
+            notifier.notify({
+                title: `${currentSong.artist} - ${currentSong.name}`,
+                message: `Rating: ${currentSong.rating/20}\nLast played: ${lastPlayed}`,
+            });
+        }
+    });
+
 
     app.on('will-quit', function () {
         // Unregister all shortcuts.
