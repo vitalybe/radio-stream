e98c07921c84e5745d9df67f3714990b73865626
Vitaly Belman <vitalyb+radio@gmail.com>
2016-06-19 21:44:04 +0300

    removing testfairy

--- android/app/src/main/java/com/vitalyb/android_music_stream/ChoosePlaylistActivity.java
+++ android/app/src/main/java/com/vitalyb/android_music_stream/ChoosePlaylistActivity.java
@@ -4,16 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.testfairy.TestFairy;
-
 public class ChoosePlaylistActivity extends AppCompatActivity implements PlaylistsFragment.OnPlaylistSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_playlist);
-
-        TestFairy.begin(this, "764a3f516e3fdab9f742b8aaf02f2058bd95890c");
     }
 
     @Override
