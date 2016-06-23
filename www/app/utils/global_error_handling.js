7f50d258924c89a4fd015addbbe3066975bff6cd
Vitaly Belman <vitalyb+radio@gmail.com>
2016-06-23 22:01:19 +0300

    web - debugger break on unhandled exceptions

--- www/app/utils/global_error_handling.js
+++ www/app/utils/global_error_handling.js
@@ -1,5 +1,6 @@
 // Global error handling
 window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+    debugger;
     alert('Unhandled thrown error: ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber
         + ' Column: ' + column + ' StackTrace: ' + errorObj);
 };
@@ -7,6 +8,7 @@ window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
 console.error = (function (old_function) {
     return function (text) {
         old_function(text);
+        debugger;
         alert("Unexpected console error: " + text)
     };
 }(console.error.bind(console)));
