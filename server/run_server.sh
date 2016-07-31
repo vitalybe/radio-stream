b46fb51e45821dd9c4c1e3f185b5ddb14cb4cfe3
Vitaly Belman <vitalyb+radio@gmail.com>
2016-07-31 22:29:02 +0300

    run server bash

---
+++ server/run_server.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+SSH_PUB=~/.ssh/id_rsa.pub
+DATA=~/Dropbox/Application\ Settings/beets_data
+MUSIC=~/Dropbox/Projects/music-stream/server/beets/test/rsrc
+
+docker run -it -p 80:80  -p 2222:22\
+    -v "$SSH_PUB":/root/.ssh/authorized_keys:ro\
+	-v "$DATA":/radio-stream/data\
+	-v "$MUSIC":/radio-stream/data/music\
+	vitalybe/radio-stream
