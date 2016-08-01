fd204fe679df89f9f253717ba7218e5875f53d91
Vitaly Belman <vitalyb+radio@gmail.com>
2016-08-01 22:35:13 +0300

    updating running scripts

--- server/run_server.sh
+++ server/run_server.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-SSH_PUB=~/.ssh/id_rsa.pub
-DATA=~/Dropbox/Application\ Settings/beets_data
+SSH_PUB=~/Dropbox/Application\ Settings/radio-stream/ssh-key/authorized_keys
+DATA=~/Dropbox/Application\ Settings/radio-stream/beets-data
 MUSIC=~/Dropbox/Projects/music-stream/server/beets/test/rsrc
 
-docker run -it -p 80:80  -p 2222:22\
+docker run -it -p 80:80  -p 22123:22\
     -v "$SSH_PUB":/root/.ssh/authorized_keys:ro\
 	-v "$DATA":/radio-stream/data\
 	-v "$MUSIC":/radio-stream/music\
