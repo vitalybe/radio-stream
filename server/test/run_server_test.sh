e9b2f796453cb8e1d55773c8efbcc03cb8f578af
Vitaly Belman <vitalyb+radio@gmail.com>
2016-08-05 00:50:33 +0300

    removing music files

--- server/test/run_server_test.sh
+++ server/test/run_server_test.sh
@@ -4,9 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SSH_PUB=~/Dropbox/Application\ Settings/radio-stream/ssh-key/authorized_keys
 DATA=$DIR/beets-data
 MUSIC=$DIR/music
+NEW_MUSIC=$DIR/new-music
 
 docker run -it -p 80:80  -p 22123:22\
     -v "$SSH_PUB":/root/.ssh/authorized_keys:ro\
 	-v "$DATA":/radio-stream/data\
 	-v "$MUSIC":/radio-stream/music\
+	-v "$NEW_MUSIC":/radio-stream/new-music\
 	vitalybe/radio-stream $*
