3fb54262d13f2e4d31d52466b233c74180cb552c
Vitaly Belman <vitalyb+radio@gmail.com>
2016-08-14 20:06:34 +0300

    cd to docker build folder on build

--- server/build_dockerfile.sh
+++ server/build_dockerfile.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 docker build -t vitalybe/radio-stream:latest .
--- server/build_dockerfile_with_base.sh
+++ server/build_dockerfile_with_base.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 docker build -t vitalybe/radio-stream-base:latest docker_radio_stream_base
 docker build -t vitalybe/radio-stream:latest .
