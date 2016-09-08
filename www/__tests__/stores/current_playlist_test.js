431a24c13dd376a01591495c3536dee6106690ef
Vitaly Belman <vitalyb+radio@gmail.com>
2016-09-08 21:39:08 +0300

    peek test

--- www/__tests__/stores/current_playlist_test.js
+++ www/__tests__/stores/current_playlist_test.js
@@ -12,28 +12,60 @@ const TITLE_2 = "T2";
 const ARTIST_2 = "A2";
 const ALBUM_2 = "M2";
 
+describe('CurrentPlaylist', () => {
 
-it('next songs returns and reloads correctly', () => {
-    backendMetadataApi.playlistSongs.mockReturnValue(new Promise(resolve => {
-        let songs = [generateMockSong(ARTIST_1, ALBUM_1, TITLE_1), generateMockSong(ARTIST_2, ALBUM_2, TITLE_2)];
-        resolve(songs);
-    }));
+    beforeEach(() => {
+        backendMetadataApi.playlistSongs.mockClear();
 
-    let playlist = new CurrentPlaylist();
-    return playlist.nextSong()
-        .then(song => {
-            return expect(song.title).toBe(TITLE_1);
-        })
-        .then(() => {
-            return playlist.nextSong()
-        })
-        .then(song => {
-            return expect(song.title).toBe(TITLE_2);
-        })
-        .then(() => {
-            return playlist.nextSong()
-        })
-        .then(song => {
-            return expect(song.title).toBe(TITLE_1);
-        });
+        backendMetadataApi.playlistSongs.mockReturnValue(new Promise(resolve => {
+            let songs = [generateMockSong(ARTIST_1, ALBUM_1, TITLE_1), generateMockSong(ARTIST_2, ALBUM_2, TITLE_2)];
+            resolve(songs);
+        }));
+    });
+
+    it('next songs returns and reloads correctly', () => {
+        let playlist = new CurrentPlaylist();
+
+        return playlist.nextSong()
+            .then(song => {
+                expect(backendMetadataApi.playlistSongs.mock.calls.length).toBe(1);
+
+                return expect(song.title).toBe(TITLE_1);
+            })
+            .then(() => {
+                return playlist.nextSong()
+            })
+            .then(song => {
+                return expect(song.title).toBe(TITLE_2);
+            })
+            .then(() => {
+                return playlist.nextSong()
+            })
+            .then(song => {
+                expect(backendMetadataApi.playlistSongs.mock.calls.length).toBe(2);
+
+                return expect(song.title).toBe(TITLE_1);
+            });
+    });
+
+    it('peek song returns the current song', () => {
+        let playlist = new CurrentPlaylist();
+
+        return playlist.peekNextSong()
+            .then(song => {
+                expect(backendMetadataApi.playlistSongs.mock.calls.length).toBe(1);
+
+                return expect(song.title).toBe(TITLE_1);
+            })
+            .then(() => {
+                return playlist.peekNextSong()
+            })
+            .then(song => {
+                return expect(song.title).toBe(TITLE_1);
+            })
+            .then(() => {
+                expect(backendMetadataApi.playlistSongs.mock.calls.length).toBe(1);
+            });
+    });
+
 });
