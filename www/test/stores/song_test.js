1a3ee4af176e33858a7c4a9f060f01da101eb9d6
Vitaly Belman <vitalyb+radio@gmail.com>
2016-09-19 23:00:49 +0300

    callback tests

--- www/test/stores/song_test.js
+++ www/test/stores/song_test.js
@@ -18,16 +18,16 @@ describe('Song', () => {
     beforeEach(() => {
 
         // Mocks
-        self.wrappedSound = {
+        self.stubWrappedSound = {
             // mobx hack - related issue: https://github.com/mobxjs/mobx/issues/421
             play: mobx.asReference(sinon.stub()),
             pause: sinon.stub(),
             loaded: true
         };
 
-        self.wrappedSoundManagerStub = {
+        self.stubWrappedSoundManager = {
             loadSound: sinon.stub().returns(new Promise(resolve => {
-                resolve(self.wrappedSound)
+                resolve(self.stubWrappedSound)
             }))
         };
 
@@ -39,7 +39,7 @@ describe('Song', () => {
 
         // SUT
         let SongModule = proxyquire('../../app/stores/song.js', {
-            '../utils/wrapped_sound_manager': self.wrappedSoundManagerStub,
+            '../utils/wrapped_sound_manager': self.stubWrappedSoundManager,
             '../utils/backend_lastfm_api': self.backendLastFmStub
         });
         self.Song = SongModule.Song;
@@ -48,18 +48,59 @@ describe('Song', () => {
     it('initialization', () => {
         console.log("song initialization");
         let song = new self.Song(generateMockSong(ARTIST, ALBUM, TITLE));
-        expect(song.title).to.be.equal(TITLE);
-        expect(song.artist).to.be.equal(ARTIST);
-        expect(song.album).to.be.equal(ALBUM)
+        expect(song.title).to.equal(TITLE);
+        expect(song.artist).to.equal(ARTIST);
+        expect(song.album).to.equal(ALBUM)
     });
 
     it('playSound happy path', () => {
         let song = new self.Song(generateMockSong(ARTIST, ALBUM, TITLE));
         return song.playSound().then(() => {
-            expect(self.wrappedSoundManagerStub.loadSound.callCount).to.be.equal(1);
-            expect(self.wrappedSound.play.callCount).to.be.equal(1);
+            expect(self.stubWrappedSoundManager.loadSound.callCount).to.equal(1);
+            expect(self.stubWrappedSound.play.callCount).to.equal(1);
         });
-
     });
 
+    it('playSound subscribe finish', () => {
+        let song = new self.Song(generateMockSong(ARTIST, ALBUM, TITLE));
+        let finishCallbackStub = sinon.stub();
+        song.subscribeFinish(finishCallbackStub);
+        return song.playSound().then(() => {
+
+            var stubPlay = self.stubWrappedSound.play;
+            expect(stubPlay.callCount).to.equal(1);
+
+            var optionsArg = stubPlay.getCall(0).args[0];
+            expect(optionsArg.onfinish).to.equal(finishCallbackStub);
+        });
+    });
+
+
+    it('playSound subscribe progress', () => {
+        let song = new self.Song(generateMockSong(ARTIST, ALBUM, TITLE));
+        let progressCallbackStub = sinon.stub();
+        song.subscribePlayProgress(progressCallbackStub);
+        return song.playSound().then(() => {
+
+            var stubPlay = self.stubWrappedSound.play;
+            expect(stubPlay.callCount).to.equal(1);
+
+            var optionsArg = stubPlay.getCall(0).args[0];
+
+            expect(progressCallbackStub.callCount).to.equal(0);
+
+            self.stubWrappedSound.position = 5000;
+            optionsArg.whileplaying.call(self.stubWrappedSound);
+            expect(progressCallbackStub.callCount).to.equal(1);
+
+            // callback shouldn't be called if position still on same second
+            self.stubWrappedSound.position = 5100;
+            optionsArg.whileplaying.call(self.stubWrappedSound);
+            expect(progressCallbackStub.callCount).to.equal(1);
+
+            self.stubWrappedSound.position = 6000;
+            optionsArg.whileplaying.call(self.stubWrappedSound);
+            expect(progressCallbackStub.callCount).to.equal(2);
+        });
+    })
 });
