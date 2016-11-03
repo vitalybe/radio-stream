c1be62c907993613d63d03a569f94281af6ff77a
Vitaly Belman <vitalyb+radio@gmail.com>
2016-11-03 21:35:37 +0200

    removed unused last.fm functions

--- radio-stream-www/app/utils/backend_lastfm_api.js
+++ radio-stream-www/app/utils/backend_lastfm_api.js
@@ -4,33 +4,6 @@ var moduleLogger = loggerCreator(__filename);
 
 import lastFmSdk, { API_KEY } from '../utils/lastfm_sdk'
 
-export function updateNowPlaying(song) {
-    let logger = loggerCreator(updateNowPlaying.name, moduleLogger);
-    logger.debug("Updating...");
-
-    lastFmSdk.track.updateNowPlaying({artist: song.artist, track: song.title}, {
-        success: function (data) {
-            logger.info("Done");
-        }, error: function (code, message) {
-            logger.error(`Failed - ${message}`);
-        }
-    });
-}
-
-export function scrobble(song) {
-    let logger = loggerCreator(scrobble.name, moduleLogger);
-    logger.debug("Scrobbling...");
-
-    var scrobbledSong = {artist: song.artist, track: song.title, timestamp: (new Date()).getTime() / 1000};
-    lastFmSdk.track.scrobble(scrobbledSong, {
-        success: function (data) {
-            logger.info("Done");
-        }, error: function (code, message) {
-            logger.error(`Failed - ${message}`);
-        }
-    });
-}
-
 export function getArtistImage(artist) {
     let logger = loggerCreator(getArtistImage.name, moduleLogger);
     logger.info("Start");
