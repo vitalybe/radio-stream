4a922b89e617ca70f2d6e60ebae4e8a68cab4184
Vitaly Belman <vitalyb+radio@gmail.com>
2016-12-04 21:52:36 +0200

    receving events and showing basic song data

--- mobile/app/pages/player_page.js
+++ mobile/app/pages/player_page.js
@@ -2,7 +2,7 @@ import loggerCreator from '../utils/logger'
 var moduleLogger = loggerCreator("player_page");
 
 import React, { Component } from 'react';
-import { StyleSheet, View, TouchableHighlight, Image, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, TouchableHighlight, Image, ActivityIndicator, DeviceEventEmitter } from 'react-native';
 
 import { colors } from '../styles/styles'
 import Button from '../components/button'
@@ -12,11 +12,25 @@ import playerProxy from '../native_proxy/player_proxy'
 
 export default class PlayerPage extends Component {
 
-  componentWillMount() {
-    this.state = {};
+  PLAYER_STATUS_EVENT = "PLAYER_STATUS_EVENT";
 
-    playerProxy.changePlaylist(this.props.playlistName)
-    // playerProxy.play();
+  componentWillMount() {
+    this.state = {
+      isLoading: true,
+      isPlaying: false,
+
+      playlistName: this.props.playlistName,
+      song: null
+    };
+
+    DeviceEventEmitter.addListener(this.PLAYER_STATUS_EVENT, event => this.onPlayerEvent(event));
+
+    playerProxy.changePlaylist(this.props.playlistName);
+    playerProxy.play();
+  }
+
+  componentWillUnmount() {
+    DeviceEventEmitter.removeAllListeners(this.PLAYER_STATUS_EVENT);
   }
 
   onPressPlayPause() {
@@ -27,21 +41,52 @@ export default class PlayerPage extends Component {
     playerProxy.playNext();
   }
 
+  onPlayerEvent(event) {
+    let logger = loggerCreator(this.onPlayerEvent.name, moduleLogger);
+    logger.info(`got event: ${JSON.stringify(event)}`);
+
+    // sample event: {"song":{"title":"Strangelove","album":"Music For the Masses","artist":"Depeche Mode"},
+    //                "playlist":{"name":"Peaceful"},"isPlaying":false,"isLoading":true}
+
+    event.song = event.song || {};
+
+    this.setState({
+      isLoading: event.isLoading,
+      isPlaying: event.isPlaying,
+      song: {
+        artist: event.song.artist,
+        title: event.song.title,
+        album: event.song.album,
+      }
+    })
+  }
+
   render() {
     let logger = loggerCreator(this.render.name, moduleLogger);
     logger.info(`start`);
 
+
+
     return (
       <View style={styles.container}>
-        <Text style={styles.playlistText}>
+        <Text>
           {this.props.playlistName}
         </Text>
-        <Button onPress={() => this.onPressPlayPause()}>
-          <Text>Play/Pause</Text>
-        </Button>
-        <Button onPress={() => this.onPressNext()}>
-          <Text>Next</Text>
-        </Button>
+        <Choose>
+          <When condition={!this.state.isLoading}>
+            <Text>{`${this.state.song.artist} - ${this.state.song.album} - ${this.state.song.title}`}</Text>
+            {/* Controls */}
+            <Button onPress={() => this.onPressPlayPause()}>
+              <Text>{this.state.isPlaying ? "Pause" : "Play"}</Text>
+            </Button>
+            <Button onPress={() => this.onPressNext()}>
+              <Text>Next</Text>
+            </Button>
+          </When>
+          <Otherwise>
+            <ActivityIndicator />
+          </Otherwise>
+        </Choose>
       </View>
     );
   }
