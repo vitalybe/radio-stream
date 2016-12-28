1c4fae38f38d1a4b888053a75d4f758e13baaecf
Vitaly Belman <vitalyb+radio@gmail.com>
2016-12-28 21:20:11 +0200

    moving art to dropbox

--- android/app/src/main/java/com/vitalyb/android_music_stream/Consts.java
+++ android/app/src/main/java/com/vitalyb/android_music_stream/Consts.java
@@ -4,7 +4,7 @@ package com.vitalyb.android_music_stream;
  * Created by vitaly on 14/03/2016.
  */
 public class Consts {
-    private static final String URL_ROOT = "***REMOVED***/5f707e4f-97cc-438e-90d8-1e5e35bd558a";
+    private static final String URL_ROOT = "http://where-is-my-music-vf7bm.duckdns.org/5f707e4f-97cc-438e-90d8-1e5e35bd558a";
     public static final String URL_API = URL_ROOT + "/api";
     public static final String URL_MUSIC = URL_ROOT + "/music";
 }
