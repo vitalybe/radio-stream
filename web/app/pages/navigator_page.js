63d59e259d1cc65fc3a96511aa305ad46bab211b
Vitaly Belman <vitalyb+radio@gmail.com>
2017-04-05 22:05:15 +0300

    removed unused top-bar with back button

--- web/app/pages/navigator_page.js
+++ web/app/pages/navigator_page.js
@@ -3,7 +3,6 @@ import React, {Component} from 'react';
 import {observer} from "mobx-react"
 import {HashRouter as Router, Route} from 'react-router-dom'
 
-import playlistCollection from '../stores/playlist_collection'
 import player from 'stores/player'
 import navigator from 'stores/navigator'
 
@@ -12,16 +11,6 @@ import {PlayerPage} from 'pages/player_page';
 import {SettingsModificationsPage} from './settings_modifications_page'
 import {FatalErrorPage} from './fatal_error_page'
 import {Sidebar} from 'components/sidebar/sidebar_index'
-
-class TopBarComponent extends Component {
-  render() {
-    return (
-      <div className="top-bar">
-      </div>
-    )
-  }
-}
-
 
 // Inspired by: https://github.com/mobxjs/mobx-contacts-list/blob/6c8e889f1bc84644d91ee0043b7c5e0a4482195c/src/app/stores/view-state.js
 @observer
@@ -36,7 +25,6 @@ export default class NavigatorPage extends Component {
     return (
       <div className="main">
         <div className="background"></div>
-        <TopBarComponent/>
         <Router>
           <div>
             <Route path="/" exact={true} component={StartUpPage}/>
