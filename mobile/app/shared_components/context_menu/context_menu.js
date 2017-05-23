a77263bf95026661022fdb540633d7ecfd027a22
Vitaly Belman <vitalyb+radio@gmail.com>
2017-05-23 17:42:12 +0300

    context menu web - Show in the correct location

--- mobile/app/shared_components/context_menu/context_menu.js
+++ mobile/app/shared_components/context_menu/context_menu.js
@@ -6,7 +6,7 @@ require("./context_menu.css");
 
 let MenuContext = props => <View>{props.children}</View>
 
-let MenuTrigger = props => props.children
+let MenuTrigger = props => <View style={props.style}>{props.children}</View>
 let MenuOptions = props => <View style={props.style}>{props.children}</View>
 let MenuOption = props => <li style={props.style}><a href="#" onClick={props.onSelect}>{props.children}</a></li>
 
@@ -23,13 +23,14 @@ class Menu extends Component {
     const menuOptions = this.props.children.find(component => component.type === MenuOptions)
 
     return (
-      <DropdownMenu
-        isOpen={this.state.contextMenuOpen}
-        close={() => this.setState({contextMenuOpen: false})}
-        toggle={<View style={this.props.style}
-                      onClick={() => this.setState({contextMenuOpen: !this.state.contextMenuOpen})}>{toggle}</View>}>
-        {menuOptions}
-      </DropdownMenu>
+      <View style={this.props.style}>
+        <DropdownMenu
+          isOpen={this.state.contextMenuOpen}
+          close={() => this.setState({contextMenuOpen: false})}
+          toggle={<View onClick={() => this.setState({contextMenuOpen: !this.state.contextMenuOpen})}>{toggle}</View>}>
+          {menuOptions}
+        </DropdownMenu>
+      </View>
     )
   }
 }
