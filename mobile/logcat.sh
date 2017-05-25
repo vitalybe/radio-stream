c949dc26d4dc7c2f98e42c6a945d1cac4064181f
Vitaly Belman <vitalyb+radio@gmail.com>
2017-05-25 17:13:20 +0300

    use lnav for log viewing

--- mobile/logcat.sh
+++ mobile/logcat.sh
@@ -1,25 +1,2 @@
 #!/usr/bin/env bash
-
-pidcat com.radiostream \
--l D \
--i ViewRootImpl \
--i DisplayListCanvas \
--i Timeline \
--i mali_winsys \
--i art \
--i libGLESv1 \
--i unknown:ViewManagerPropertyUpdater \
--i ApkSoSource \
--i TimaKeyStoreProvider \
--i ActivityThread \
--i ResourcesManager \
--i InjectionManager \
--i SensorManager \
--i Activity \
--i OpenGLRenderer \
--i SecWifiDisplayUtil \
--i libEGL \
--i Zygote \
--i SELinux \
--i System.out \
--i MediaHTTPConnection
+adb logcat -v time "*:F" RadioStream:V | lnav
