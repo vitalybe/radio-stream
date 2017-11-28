00865af68efc087cb367e14ed74bd65ee493e3ba
Vitaly Belman <vitaly@telefonica.com>
2017-11-28 09:33:35 +0200

    prettier metadata UI

--- app/js/app/pages/player_page/content/metadata_content.js
+++ app/js/app/pages/player_page/content/metadata_content.js
@@ -7,6 +7,7 @@ import { Image, StyleSheet, Text, View } from "react-native";
 import { observer } from "mobx-react";
 
 import NormalText from "app/shared_components/text/normal_text";
+import BigText from "app/shared_components/text/big_text";
 import moment from "moment";
 import contentStyle from "./content_style";
 
@@ -14,8 +15,15 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
   },
-  metadataInfo: {
+  header: {
+    marginBottom: 15,
+  },
+  line: {
     marginBottom: 5,
+    fontWeight: "bold",
+  },
+  value: {
+    fontWeight: "normal",
   },
 });
 
@@ -26,9 +34,16 @@ export default class MetadataContent extends Component {
 
     return (
       <View style={[contentStyle.container, styles.container, this.props.style]}>
-        <NormalText style={styles.metadataInfo}>Last played: {moment.unix(song.lastplayed).fromNow()}</NormalText>
-        <NormalText style={styles.metadataInfo}>Play count: {song.playcount}</NormalText>
-        <NormalText style={styles.metadataInfo}>Marked as played: {song.isMarkedAsPlayed ? "✔" : "x"}</NormalText>
+        <BigText style={styles.header}>Metadata</BigText>
+        <NormalText style={styles.line}>
+          Last played: <NormalText style={styles.value}>{moment.unix(song.lastplayed).fromNow()}</NormalText>
+        </NormalText>
+        <NormalText style={styles.line}>
+          Play count: <NormalText style={styles.value}>{song.playcount}</NormalText>
+        </NormalText>
+        <NormalText style={styles.line}>
+          Marked as played: <NormalText style={styles.value}>{song.isMarkedAsPlayed ? "✔" : "x"}</NormalText>
+        </NormalText>
       </View>
     );
   }
