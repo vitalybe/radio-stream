62f6ad860bff9cdcc807261d49b12350fd0c2a49
Sergio <helpse@gmail.com>
2018-04-18 03:30:02 -0500

    upd build_dockerfile

--- server/build_dockerfile.sh
+++ server/build_dockerfile.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 $DIR/cli/bin/server/stop
-docker build -t vitalybe/radio-stream:latest .
+docker build -t helpse/radio-stream:latest .
